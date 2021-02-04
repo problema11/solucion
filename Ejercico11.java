2a5d95577ed34bc2178eed61e53396fbdd369a25
aldahirLN <78501195+aldahirLN@users.noreply.github.com>
2021-02-03 20:25:36 -0600

    Update Ejercico11.java

--- Ejercico11.java
+++ Ejercico11.java
@@ -10,13 +10,47 @@ public class Ejercico11 {
     public static void main(String[] args) {
         EmpTrans emp=new EmpTrans ("El rápido");
         
-        emp.altaCamion("Mercedes", "5432g", "d355", 500000, 20, true, true);
-        emp.altaCamion("Mercedes", "54324g", "d3545", 5000000, 40, true, false);
-        emp.altaCamion("audi", "543gt", "t564", 200000, 30, true);
-        emp.altaCamion("audi", "5432gg", "d354355", 500000, 20, false, true);
-        emp.altaCamion("LOl", "5432g3g", "df355", 500000, 20, true, false);
+        if(emp.altaCamion("Mercedes", "5432g", "d355", 500000, 20, true, true))
+            System.out.println("Alta exitosa");
+        else
+            System.out.println("Error en el alta");
         
+        if(emp.altaCamion("Mercedes", "54324g", "d3545", 5000000, 40, true, false))
+            System.out.println("Alta exitosa");
+        else
+            System.out.println("Error en el alta");
+        
+        if(emp.altaCamion("audi", "543gt", "t564", 200000, 30, true))
+            System.out.println("Alta exitosa");
+        else
+            System.out.println("Error en el alta");
+        
+        if(emp.altaCamion("audi", "5432gg", "d354355", 500000, 20, true))
+            System.out.println("Alta exitosa");
+        else
+            System.out.println("Error en el alta");
+        
+        if(emp.altaCamion("LOl", "5432g3g", "df355", 500000, 20, true, false))
+            System.out.println("Alta exitosa");
+        else
+            System.out.println("Error en el alta");
+        
+        //inciso1
         System.out.println(emp.buscaTurismo(10, 300));
+        System.out.println("_____________________________");
+        //inciso2
+        System.out.println(emp.buscaEscolares(15, 2));
+        System.out.println("_____________________________");
+        //inciso3
+        System.out.println(emp.buscarPlaca("d355"));
+        System.out.println(emp.buscarPlaca("t564"));
+        System.out.println("_____________________________");
+        //inciso4
+        System.out.println(emp.camionesEscolaresDisponibles());
+        System.out.println("_____________________________");
+        //inciso5
+        System.out.println(emp.totalMarcaTurismo("Mercedes",300, 5000));
+        
     }
     
 }
