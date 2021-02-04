77fbfed268ea9ebdb63acc4f6906f002cbddc8d9
emiruilop <78506235+emiruilop@users.noreply.github.com>
2021-02-03 18:14:01 -0600

    Update EmpTrans.java

--- EmpTrans.java
+++ EmpTrans.java
@@ -107,6 +107,6 @@ public class EmpTrans {
         int i=0;
         
         while(i<totalCam)
-            if(camiones[i] instanceof Turismo && ((Turismo)camiones[i]).getMarca().equals(marca) && 
+            if(camiones[i] instanceof Turismo && ((Turismo)camiones[i]).getMarca().equals(marca) && ((Turismo)camiones[i]).serviBar() == true && ((Turismo)camiones[i]).calculaCostoServicio
     
 }
